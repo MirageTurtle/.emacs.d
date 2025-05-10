46818da352592279d3b3ec819fdf2a1818917b05
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2025-05-10 12:19:55 +0800

    feat: copy back `treesit-language-source-alist`.

--- lisp/init-treesit.el
+++ lisp/init-treesit.el
@@ -8,6 +8,16 @@
 (use-package treesit-fold
   :straight (treesit-fold :type git :host github :repo "emacs-tree-sitter/treesit-fold")
   :after treesit
+  :init
+  (setq treesit-language-source-alist
+        '((elisp      . ("https://github.com/Wilfred/tree-sitter-elisp"))
+          (rust       . ("https://github.com/tree-sitter/tree-sitter-rust"))
+          (toml       . ("https://github.com/tree-sitter/tree-sitter-toml"))
+          (go         . ("https://github.com/tree-sitter/tree-sitter-go"))
+          (gomod      . ("https://github.com/camdencheek/tree-sitter-go-mod"))
+          (python     . ("https://github.com/tree-sitter/tree-sitter-python"))
+          (javascript . ("https://github.com/tree-sitter/tree-sitter-javascript"))
+          (typescript . ("https://github.com/tree-sitter/tree-sitter-typescript"))))
   :hook
   (prog-mode . treesit-fold-mode)
   :bind
