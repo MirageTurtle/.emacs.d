535482507548bbed9424949d601d99237e75f793
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2025-07-28 18:24:31 +0800

    chore: change the projectile-completion-system to auto.

--- lisp/init-proj.el
+++ lisp/init-proj.el
@@ -9,7 +9,7 @@
   (setq projectile-switch-project-action #'projectile-dired)
   (setq projectile-enable-caching t)
   (setq projectile-indexing-method 'native)
-  (setq projectile-completion-system 'ivy)
+  (setq projectile-completion-system 'auto)
   :config
   (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map) ; keybinding for projectile commands to override default C-x p
   )
