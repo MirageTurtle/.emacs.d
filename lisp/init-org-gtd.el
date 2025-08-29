2282e91f5b894dff91efb5583c8a2932ee7ba5d5
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2025-08-29 19:01:33 +0800

    org: rebuild my org configuration with init-org-gtd.el.

---
+++ lisp/init-org-gtd.el
@@ -0,0 +1,25 @@
+;;; init-org-gtd.el --- org-mode-based GTD configuration -*- lexical-binding: t; -*-
+;;; Original article: https://web.archive.org/web/20170916093203/http://emacs.cafe/emacs/orgmode/gtd/2017/06/30/orgmode-gtd.html
+;;; Original article (Chinese): https://grass.show/translate/do-gtd-in-orgmode/
+
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c C-w") 'org-refile)
+(setq org-agenda-files '("~/gtd/inbox.org"
+                         "~/gtd/gtd.org"
+                         "~/gtd/tickler.org"))
+
+(setq org-capture-templates '(("t" "Todo [inbox]" entry
+                               (file+headline "~/gtd/inbox.org" "Tasks")
+                               "* TODO %i%?")
+                              ("T" "Tickler" entry
+                               (file+headline "~/gtd/tickler.org" "Tickler")
+                               "* %i%?\n %U")))
+
+(setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
+                           ("~/gtd/someday.org" :level . 1)
+                           ("~/gtd/tickler.org" :maxlevel . 2)))
+
+(setq org-todo-keywords '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d)" "CANC(c)")))
+
+(provide 'init-org-gtd)
+;;; init-org-gtd.el ends here
