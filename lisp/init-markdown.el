de8ddb90db6b48006730885f8281e276855c7469
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2026-01-13 16:15:24 +0800

    feat(markdown): switch from markdown-mode to markdown-ts-mode with tree-sitter support.

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -3,8 +3,11 @@
 
 ;;; Code:
 
-(use-package markdown-mode
-  :straight t)
+(use-package markdown-ts-mode
+  :straight t
+  :config
+  (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
+  (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src")))
 
 (provide 'init-markdown)
 
