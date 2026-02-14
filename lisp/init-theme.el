6e83068a35b4dc0d89940c4558c3142c1fdc97d8
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2026-02-14 16:14:47 +0800

    refactor(theme): comment out awesome-tray configuration
    Remove awesome-tray package configuration while preserving it as reference code for potential future use.

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -51,43 +51,43 @@
 ;; and I do not have ability to add a icon to show the status of meow,
 ;; so I changed to doom-modeline.
 ;; I keep the configuration here for (maybe future) reference.
-(use-package awesome-tray
-  :straight (awesome-tray :type git :host github :repo "manateelazycat/awesome-tray")
-  ;; :straight t
-  :config
-  ;; (awesome-tray-mode 1)
-  (setq awesome-tray-active-modules '("location" "file-path" "buffer-name" "mode-name" "git" "battery" "date"))
-  (setq awesome-tray-buffer-name-buffer-changed t)
-  (setq awesome-tray-date-format "%Y-%m-%d %H:%M %a")
-  (defun awesome-tray-git-command-update-cache ()
-    "Remove the space between the branch name and the status symbol, based on the original function."
-    (if (file-exists-p (format "%s" (buffer-file-name)))
-	(let* ((filename (buffer-file-name))
-               (status (vc-git-state filename))
-               (branch (car (vc-git-branches))))
+;; (use-package awesome-tray
+;;   :straight (awesome-tray :type git :host github :repo "manateelazycat/awesome-tray")
+;;   ;; :straight t
+;;   :config
+;;   ;; (awesome-tray-mode 1)
+;;   (setq awesome-tray-active-modules '("location" "file-path" "buffer-name" "mode-name" "git" "battery" "date"))
+;;   (setq awesome-tray-buffer-name-buffer-changed t)
+;;   (setq awesome-tray-date-format "%Y-%m-%d %H:%M %a")
+;;   (defun awesome-tray-git-command-update-cache ()
+;;     "Remove the space between the branch name and the status symbol, based on the original function."
+;;     (if (file-exists-p (format "%s" (buffer-file-name)))
+;; 	(let* ((filename (buffer-file-name))
+;;                (status (vc-git-state filename))
+;;                (branch (car (vc-git-branches))))
 
-          (pcase status
-            ('up-to-date (setq status ""))
-            ('edited (setq status "!"))
-            ('needs-update (setq status "⇣"))
-            ('needs-merge (setq status "⇡"))
-            ('unlocked-changes (setq status ""))
-            ('added (setq status "+"))
-            ('removed (setq status "-"))
-            ('conflict (setq status "="))
-            ('missing (setq status "?"))
-            ('ignored (setq status ""))
-            ('unregistered (setq status "?"))
-            (_ (setq status "")))
-          (if (not branch) (setq branch ""))
+;;           (pcase status
+;;             ('up-to-date (setq status ""))
+;;             ('edited (setq status "!"))
+;;             ('needs-update (setq status "⇣"))
+;;             ('needs-merge (setq status "⇡"))
+;;             ('unlocked-changes (setq status ""))
+;;             ('added (setq status "+"))
+;;             ('removed (setq status "-"))
+;;             ('conflict (setq status "="))
+;;             ('missing (setq status "?"))
+;;             ('ignored (setq status ""))
+;;             ('unregistered (setq status "?"))
+;;             (_ (setq status "")))
+;;           (if (not branch) (setq branch ""))
 
-          (setq awesome-tray-git-buffer-filename filename)
+;;           (setq awesome-tray-git-buffer-filename filename)
 
-          (setq awesome-tray-git-command-cache (if awesome-tray-git-show-status
-                                                   (format awesome-tray-git-format (string-trim (concat branch "" status)))
-						 (format awesome-tray-git-format branch))))
-      (setq awesome-tray-git-buffer-filename nil
-            awesome-tray-git-command-cache ""))))
+;;           (setq awesome-tray-git-command-cache (if awesome-tray-git-show-status
+;;                                                    (format awesome-tray-git-format (string-trim (concat branch "" status)))
+;; 						 (format awesome-tray-git-format branch))))
+;;       (setq awesome-tray-git-buffer-filename nil
+;;             awesome-tray-git-command-cache ""))))
 
 (provide 'init-theme)
 
