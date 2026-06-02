44ebca13083a7ceb65c2c89e3426dda3ea9ac3f3
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2026-06-02 18:52:30 +0800

    feat(markdown): use keybinding instead of mode hook

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -9,11 +9,12 @@
   (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
   (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src")))
 
+;; grip-mode for preview markdown files using go-grip
 (use-package grip-mode
   :straight t
   :config (setq grip-command 'go-grip)
-  :hook ((markdown-mode org-mode) . grip-mode)
-  :hook ((markdown-ts-mode org-mode) . grip-mode))
+  :bind (:map markdown-mode-command-map
+              ("g" . grip-mode)))
 
 (provide 'init-markdown)
 
