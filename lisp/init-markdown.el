731331e288e95d93c7c1dc89ed6cd3ab9bc01829
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2026-06-02 13:16:20 +0800

    feat: add grip-mode for markdown preview

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -9,6 +9,12 @@
   (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
   (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src")))
 
+(use-package grip-mode
+  :straight t
+  :config (setq grip-command 'go-grip)
+  :hook ((markdown-mode org-mode) . grip-mode)
+  :hook ((markdown-ts-mode org-mode) . grip-mode))
+
 (provide 'init-markdown)
 
 ;;; init-markdown.el ends here
