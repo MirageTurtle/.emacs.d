78f6a498d00d6e11ea7801bdbf5f3e7f24a5e608
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2026-09-18 17:10:01 +0800

    refactor(markdown): use built-in markdown-ts-mode

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -4,10 +4,11 @@
 ;;; Code:
 
 (use-package markdown-ts-mode
-  :straight t
+  :straight (:type built-in)
+  :mode (("\\.md\\'"       . markdown-ts-mode)
+         ("\\.markdown\\'" . markdown-ts-mode))
   :config
-  (add-to-list 'treesit-language-source-alist '(markdown "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown/src"))
-  (add-to-list 'treesit-language-source-alist '(markdown-inline "https://github.com/tree-sitter-grammars/tree-sitter-markdown" "split_parser" "tree-sitter-markdown-inline/src")))
+  (require 'markdown-ts-mode-x))
 
 ;; grip-mode for preview markdown files using go-grip
 (use-package grip-mode
