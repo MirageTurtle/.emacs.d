72ea0ba104d757ce47c3cfff07fa5e19c9f391b5
MirageTurtle <60972592+MirageTurtle@users.noreply.github.com>
2026-09-18 17:15:27 +0800

    feat(tab-bar): add select-or-create tab behavior

--- lisp/init-tab-bar.el
+++ lisp/init-tab-bar.el
@@ -12,6 +12,20 @@
   (tab-bar-tab-name-truncated-max 20)
   (tab-bar-auto-width nil)
   :config
+  (defun mt/tab-bar-select-or-create-tab (&optional tab-number)
+    "Select TAB-NUMBER, creating it when it immediately follows the last tab."
+    (interactive "P")
+    (unless (integerp tab-number)
+      (let ((key (event-basic-type last-command-event)))
+        (setq tab-number
+              (if (and (characterp key) (<= ?1 key ?9))
+                  (- key ?0)
+                0))))
+    (if (= tab-number
+           (1+ (length (funcall tab-bar-tabs-function))))
+        (tab-bar-new-tab-to tab-number)
+      (tab-bar-select-tab tab-number)))
+
   (defun mt/tab-bar-tab-name-format-function (tab i)
     "Return the name for TAB with index I, prefixing a * when the current buffer is modified.
 The star is shown only on the selected tab (cheap & reliable)."
@@ -26,6 +40,9 @@ The star is shown only on the selected tab (cheap & reliable)."
        (propertize num 'face `(:inherit ,face :weight ultra-bold :underline t))
        (propertize (concat " " (alist-get 'name tab) " ") 'face face))))
   (setq tab-bar-tab-name-format-function #'mt/tab-bar-tab-name-format-function)
+  (define-key tab-bar-mode-map
+              [remap tab-bar-select-tab]
+              #'mt/tab-bar-select-or-create-tab)
   )
 
 (provide 'init-tab-bar)
